9371c3ffd68d0ceb6fc8767af69400ea8147982f
KyleZ1129 <68307390+KyleZ1129@users.noreply.github.com>
2021-07-21 16:19:39 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// ================= START BOT CODE ===================
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+// You really don't want your token here since your repl's code
+// is publically available. We'll take advantage of a Repl.it 
+// feature to hide the token we got earlier. 
+client.login(process.env.DISCORD_TOKEN);
